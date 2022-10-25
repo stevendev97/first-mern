f608f56a7601ffae932be2f79ac5792dc3e27b32
stevendev97 <x.limsteven@gmail.com>
2022-10-25 12:30:55 -0400

    home page modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Home (){
+export default function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className='container'>
+            <div className="option-wrapper">
+                <h1>Home Page</h1>
+                <p>Click Login if you have an account already</p>
+                <div className='button-wrapper'>
+                    <Link to="/login">
+                        <button>Login</button>
+                    </Link>
+                    <Link to="/register">
+                        <button>Register</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
